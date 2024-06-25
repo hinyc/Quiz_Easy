8409e41b360f429fbbd650987d67e3e79bf6440e
hinyc <dlsduf@gmail.com>
2024-06-26 00:15:59 +0900

    깃 커밋테스트

--- app/api/quiz/route.ts
+++ app/api/quiz/route.ts
@@ -4,7 +4,7 @@ const GET = async (req: NextRequest) => {
   const category = req.nextUrl.searchParams.get("category");
   console.log("Hello, world!", category);
 
-  return new Response("Hello, world!!");
+  return new Response("Hello, world!!!");
 };
 
 export { GET };
