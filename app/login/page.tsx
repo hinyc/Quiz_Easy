4be64f45f820a9582d55c5b2d80b92f2af126774
hinyc <dlsduf@gmail.com>
2024-08-22 23:48:15 +0900

    fix: nextauth secreat .env에 추가 및 api 테스트 버튼 추가

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,25 +1,41 @@
-'use client';
-import React, { use, useEffect, useState } from 'react';
-import { LoginPageStyle } from './page.style';
-import GoogleLoginButton from './_component/GoogleLoginButton';
-import Splash from './_component/Splash';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import { ROUTE } from '../common/link';
+"use client";
+import React, { use, useEffect, useState } from "react";
+import { LoginPageStyle } from "./page.style";
+import GoogleLoginButton from "./_component/GoogleLoginButton";
+import Splash from "./_component/Splash";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { ROUTE } from "../common/link";
+import axios from "axios";
 
 export default function LoginPage() {
-  const storedSession = typeof window !== 'undefined' ? localStorage.getItem('session') : null;
+  const storedSession =
+    typeof window !== "undefined" ? localStorage.getItem("session") : null;
 
   const { status } = useSession();
   const router = useRouter();
   useEffect(() => {
-    if (status === 'authenticated') {
+    if (status === "authenticated") {
       router.push(ROUTE.beeHive);
     }
   }, [router, status]);
   return (
     <LoginPageStyle>
       <Splash localSession={storedSession} />
+      <button
+        onClick={() => {
+          axios
+            .get("/api/quiz")
+            .then((res) => {
+              console.log(res.data);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }}
+      >
+        test
+      </button>
       <GoogleLoginButton />
     </LoginPageStyle>
   );
